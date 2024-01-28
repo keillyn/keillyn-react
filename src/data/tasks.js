1a6c9cbdf1b0b6f65d461776616b956208d53fca
Victor Bravo <victormanuelbravo1999@gmail.com>
2024-01-27 22:07:40 -0400

    fist commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primera tarea',
+        description: 'Mi primera tarea descripcion'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Mi segunda tarea descripcion'
+    },
+    {
+        id: 2,
+        title: 'Mi tercera actividad',
+        description: 'Mi tercera actividad descripcion'
+    }, 
+    {
+        id: 3,
+        title: 'Mi cuarto actividad',
+        description: 'Mi cuarta actividad descripcion'
+    },
+];
